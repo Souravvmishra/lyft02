fd3b35dec451f89bd82b09067b486917d69000b6
SouravvMishra <souravvmishra@gmail.com>
2023-07-20 22:08:28 +0530

    Completed

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -6,7 +6,7 @@ import WhiteBtn from './WhiteBtn';
 
 const Landing: React.FC = () => {
     return (
-        <div className={`xl:py-20 xl:px-40 xl:-mx-2 py-10 flex items-center space-x-4 flex-col-reverse xl:flex-row px-4 dark:bg-black/80 dark:text-white`}>
+        <div className={`xl:py-20 xl:px-40 -mx-2 py-10 flex items-center space-x-4 flex-col-reverse xl:flex-row px-4 dark:bg-black/80 dark:text-white`}>
             <div className='xl:w-[60%] my-4 '>
                 <Image src={'https://images.ctfassets.net/q8mvene1wzq4/1V7KEdm8GVuIRx1fHaCovZ/eafdba3bb2b13c73c0f79f2533a4fb6e/home_pinkpaint_hero.jpg?w=1000&q=60&fm=webp'} width={1080} height={720} alt='main' />
             </div>
